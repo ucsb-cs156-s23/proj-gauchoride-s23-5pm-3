27b920e161f693e8e6fbbbc9c4512159b35c7c44
Sean Oh <seanoh@ucsb.edu>
2023-05-24 18:13:50 -0700

    added fixtures for riderequests

---
+++ frontend/src/fixtures/rideRequestFixtures.js
@@ -0,0 +1,48 @@
+const rideRequestFixtures = {
+    oneRideRequest: {
+        "day": "Monday",
+        "fullName": "Bob John",
+        "course": "Math 4A",
+        "startTime": "11:00AM",
+        "stopTime": "12:15PM",
+        "building": "IV Theatre",
+        "room": "1",
+        "pickup": "San Rafael Residence Hall Tower"
+    },
+    threeRideRequests: [
+        {
+            "day": "Tuesday",
+            "fullName": "Tim Smith",
+            "course": "ANTH 2",
+            "startTime": "8:00AM",
+            "stopTime": "9:15PM",
+            "building": "Music",
+            "room": "LLCH",
+            "pickup": "Lot 3"
+        },
+        {
+            "day": "Wednesday",
+            "fullName": "Peter Parker",
+            "course": "CHEM 1B",
+            "startTime": "9:30AM",
+            "stopTime": "10:45PM",
+            "building": "Chemistry",
+            "room": "1179",
+            "pickup": "Anacapa Residence Hall"
+        },
+        {
+            "day": "Thursday",
+            "fullName": "Lisa Simpson",
+            "course": "ENGL 23",
+            "startTime": "9:30AM",
+            "stopTime": "10:45PM",
+            "building": "Campbell Hall",
+            "room": "",
+            "pickup": "Lot 1"
+        }
+    ]
+
+
+}
+
+export default rideRequestFixtures

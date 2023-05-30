f3b13298b11b546d168eacdf82f851a48e058209
Firoc <jasonvu@ucsb.edu>
2023-05-30 15:13:13 -0700

    WIP rideedit/test

--- frontend/src/main/pages/RideRequests/RideEditPage.js
+++ frontend/src/main/pages/RideRequests/RideEditPage.js
@@ -1,3 +1,81 @@
+// WIP c/p from team03, need to edit/change
+
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import BookForm from "main/components/Books/BookForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+
+export default function BooksEditPage() {
+  let { id } = useParams();
+
+  const { data: Book, error, status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/books?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/books`,
+        params: {
+          id
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (Book) => ({
+    url: "/api/books",
+    method: "PUT",
+    params: {
+      id: Book.id,
+    },
+    data: {
+      title: Book.title,
+      author: Book.author,
+      genre: Book.genre
+    }
+  });
+
+  const onSuccess = (Book) => {
+    toast(`Book Updated - id: ${Book.id} title: ${Book.title} author: ${Book.author} genre: ${Book.genre}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/books?id=${id}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/Books/list" />
+  }
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Edit Book</h1>
+        {Book &&
+          <BookForm initialContents={Book} submitAction={onSubmit} buttonLabel="Update" />
+        }
+      </div>
+    </BasicLayout>
+  )
+}
+
+
+/*
+
+// accidentally used team01, but going to change to team03 stuff
+// keeping the below as reference
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
 import { rideUtils }  from 'main/utils/rideUtils';
@@ -27,3 +105,5 @@ export default function RideEditPage() {
         </BasicLayout>
     )
 }
+
+*/
--- frontend/src/tests/pages/RideRequests/RideEditPage.test.js
+++ frontend/src/tests/pages/RideRequests/RideEditPage.test.js
@@ -1,20 +1,186 @@
-import { render, screen, act, waitFor, fireEvent } from "@testing-library/react";
-import RideEditPage from "main/pages/Rides/RideEditPage";
+// c/p team03, need to edit
+
+import { fireEvent, queryByTestId, render, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
+import BookEditPage from "main/pages/Books/BookEditPage";
+
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
 import mockConsole from "jest-mock-console";
 
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
 const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        useParams: () => ({
+            id: 17
+        }),
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useParams: () => ({
-        id: 3
-    }),
-    useNavigate: () => mockNavigate
-}));
+describe("BookEditPage tests", () => {
 
-const mockUpdate = jest.fn();
+    describe("when the backend doesn't return a book", () => {
+
+        const axiosMock = new AxiosMockAdapter(axios);
+
+        beforeEach(() => {
+            axiosMock.reset();
+            axiosMock.resetHistory();
+            axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+            axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+            axiosMock.onGet("/api/books", { params: { id: 17 } }).timeout();
+        });
+
+        const queryClient = new QueryClient();
+        test("renders header but table is not present", async () => {
+
+            const restoreConsole = mockConsole();
+
+            const {getByText, queryByTestId, findByText} = render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <BookEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+            await findByText("Edit Book");
+            expect(queryByTestId("BookForm-title")).not.toBeInTheDocument();
+            restoreConsole();
+        });
+    });
+
+    describe("tests where backend is working normally", () => {
+
+        const axiosMock = new AxiosMockAdapter(axios);
+
+        beforeEach(() => {
+            axiosMock.reset();
+            axiosMock.resetHistory();
+            axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+            axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+            axiosMock.onGet("/api/books", { params: { id: 17 } }).reply(200, {
+                id: 17,
+                title: 'IT',
+                author: "Stephen King",
+                genre: "Horror"
+            });
+            axiosMock.onPut('/api/books').reply(200, {
+                id: "17",
+                title: 'IT2',
+                author: "Stephen King2",
+                genre: "Horror2"
+            });
+        });
+
+        const queryClient = new QueryClient();
+        test("renders without crashing", () => {
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <BookEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+        });
+
+        test("Is populated with the data provided", async () => {
+
+            const { getByTestId, findByTestId } = render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <BookEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+
+            await findByTestId("BookForm-title");
+
+            const idField = getByTestId("BookForm-id");
+            const titleField = getByTestId("BookForm-title");
+            const authorField = getByTestId("BookForm-author");
+            const genreField = getByTestId("BookForm-genre");
+            const submitButton = getByTestId("BookForm-submit");
+
+            expect(idField).toHaveValue("17");
+            expect(titleField).toHaveValue("IT");
+            expect(authorField).toHaveValue("Stephen King");
+            expect(genreField).toHaveValue("Horror");
+        });
+
+        test("Changes when you click Update", async () => {
+
+
+
+            const { getByTestId, findByTestId } = render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <BookEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+
+            await findByTestId("BookForm-title");
+
+            const idField = getByTestId("BookForm-id");
+            const titleField = getByTestId("BookForm-title");
+            const authorField = getByTestId("BookForm-author");
+            const genreField = getByTestId("BookForm-genre");
+            const submitButton = getByTestId("BookForm-submit");
+
+            expect(idField).toHaveValue("17");
+            expect(titleField).toHaveValue("IT");
+            expect(authorField).toHaveValue("Stephen King");
+            expect(genreField).toHaveValue("Horror");
+
+            expect(submitButton).toBeInTheDocument();
+
+            fireEvent.change(titleField, { target: { value: 'IT2' } })
+            fireEvent.change(authorField, { target: { value: 'Stephen King2' } })
+            fireEvent.change(genreField, { target: { value: "Horror2" } })
+
+            fireEvent.click(submitButton);
+
+            await waitFor(() => expect(mockToast).toBeCalled);
+            expect(mockToast).toBeCalledWith("Book Updated - id: 17 title: IT2 author: Stephen King2 genre: Horror2");
+            expect(mockNavigate).toBeCalledWith({ "to": "/Books/list" });
+
+            expect(axiosMock.history.put.length).toBe(1); // times called
+            expect(axiosMock.history.put[0].params).toEqual({ id: 17 });
+            expect(axiosMock.history.put[0].data).toBe(JSON.stringify({
+                title: 'IT2',
+                author: "Stephen King2",
+                genre: "Horror2"
+            })); // posted object
+
+        });
+
+       
+    });
+});
+
+
+/*
+
+// from team01, going to remove, but keeping some as reference
+
 jest.mock('main/utils/rideUtils', () => {
     return {
         __esModule: true,
@@ -38,6 +204,7 @@ jest.mock('main/utils/rideUtils', () => {
 
 
 
+
 describe("RideEditPage tests", () => {
 
     const queryClient = new QueryClient();
@@ -46,7 +213,7 @@ describe("RideEditPage tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <RideEditPage />
+                    <ParkEditPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
@@ -57,7 +224,7 @@ describe("RideEditPage tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <RideEditPage />
+                    <ParkEditPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
@@ -93,7 +260,7 @@ describe("RideEditPage tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <RideEditPage />
+                    <ParkEditPage />
                 </MemoryRouter>
             </QueryClientProvider>
         )
@@ -136,3 +303,4 @@ describe("RideEditPage tests", () => {
     });
 
 });
+*/

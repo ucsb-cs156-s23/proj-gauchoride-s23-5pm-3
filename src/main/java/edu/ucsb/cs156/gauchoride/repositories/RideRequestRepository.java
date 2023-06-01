07ba5b5d02cff825d91a87e3fd125e1f83fe520d
Ivan Ke <yke@umail.ucsb.edu>
2023-05-31 17:19:13 -0700

    Added database for Ride Requests

---
+++ src/main/java/edu/ucsb/cs156/gauchoride/repositories/RideRequestRepository.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.gauchoride.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.ucsb.cs156.gauchoride.entities.RideRequest;
+
+import java.util.Optional;
+
+@Repository
+public interface RideRequestRepository extends CrudRepository<RideRequest, Long> {
+    Iterable<RideRequest> findAllByUserid(Long userid);
+}
